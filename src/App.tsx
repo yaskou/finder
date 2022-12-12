1d4ed904c5ff265f6903a276d974fd63beab5cfa
yaskou <anmitutarow@gmail.com>
2022-12-12 21:40:25 +0900

    feat: Add component to show local stream switching area

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import useHook from './App.hook'
+import LocalStream from './components/LocalStream'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { cameraId, onClick } = useHook()
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <article onClick={onClick} className="h-[100svh] bg-zinc-900 p-6 flex justify-center items-center">
+      <LocalStream cameraId={cameraId} />
+    </article>
   )
 }
 
